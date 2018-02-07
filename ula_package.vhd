d83bfd8217f92ab81a3102a0252a417dc547bcba
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2018-02-07 18:21:31 -0300

    Funfou
    Ta funcionadno agora. Falta as quatro ultimas operações.

--- ula_package.vhd
+++ ula_package.vhd
@@ -1,10 +1,11 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
-USE work.ula_package.all;
 
-ENTITY ula IS
-	PORT
+
+PACKAGE ula_package IS
+	COMPONENT ula_struct IS 
+		PORT
 	(
 		operacao : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 		operA	: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
@@ -13,14 +14,5 @@ ENTITY ula IS
 		Cin		: IN STD_LOGIC;
 		N,Z,C,B,V : buffer STD_LOGIC
 	);
-END ula;
-
-
-ARCHITECTURE ula OF ula IS
-
-		BEGIN
-		
-		startUla : ula_struct PORT MAP (operA, operB, operacao,result,Cin);
-		
-
-END ula;
+	END COMPONENT;
+END ula_package;
